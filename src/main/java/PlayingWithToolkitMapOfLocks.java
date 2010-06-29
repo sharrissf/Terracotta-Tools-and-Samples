db1baed73b92b5b4a6514e5eae1ca3d9fb91f1b9
Steve Harris <sharrissf@gmail.com>
2010-06-29 09:45:31 -0700

    somehow missed this file

---
+++ src/main/java/PlayingWithToolkitMapOfLocks.java
@@ -0,0 +1,54 @@
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReadWriteLock;
+
+import org.terracotta.api.ClusteringToolkit;
+import org.terracotta.api.TerracottaClient;
+import org.terracotta.collections.ClusteredMap;
+import org.terracotta.coordination.Barrier;
+
+/**
+ * 
+ * @author steve
+ * 
+ *         Simple demonstration of using cyclic barrier and reentrant read-write
+ *         locks striped over a hashmap
+ * 
+ *         Usage: PlayingWithMapOfLocks <numberOfNodes>
+ * 
+ */
+public class PlayingWithToolkitMapOfLocks {
+
+	public static void main(String[] args) {
+
+		final String barrierName = "mapOfLocksBarrier";
+
+		final int numberOfParties = Integer.parseInt(args.length == 0 ? "2"
+				: args[0]);
+		TerracottaClient client = new TerracottaClient("localhost:9510");
+		ClusteringToolkit clustering = client.getToolkit();
+
+		ClusteredMap<String, Object> map = clustering.getMap("MyMap");
+		Barrier barrier = clustering.getBarrier(barrierName, numberOfParties);
+		ReadWriteLock l = clustering.getReadWriteLock(clustering
+				.getClusterInfo().getUniversallyUniqueClientID());
+		map.put(clustering.getClusterInfo().getUniversallyUniqueClientID(), l);
+
+		try {
+			System.out.println("Waiting ...");
+			int index = barrier.await();
+			Set<String> keys = (Set<String>) map.keySet();
+			for (String k : keys) {
+
+				ReadWriteLock mrwl = (ReadWriteLock) map.get(k);
+
+				System.out.println("MAP Locked: " + k + ": "
+						+ mrwl.writeLock().tryLock(1, TimeUnit.SECONDS));
+			}
+			barrier.await();
+			System.out.println("... finished " + index);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
