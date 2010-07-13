052fea334192fbd64812c72511a04b3f63ca27a3
Steve Harris <sharrissf@gmail.com>
2010-07-12 19:41:06 -0700

    fix to fairness issue with maps sample

--- src/main/java/PlayingWithToolkitMapOfLocks.java
+++ src/main/java/PlayingWithToolkitMapOfLocks.java
@@ -1,9 +1,11 @@
-import java.util.Set;
+import java.util.Collection;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReadWriteLock;
 
 import org.terracotta.api.ClusteringToolkit;
 import org.terracotta.api.TerracottaClient;
+import org.terracotta.cluster.ClusterNode;
 import org.terracotta.collections.ClusteredMap;
 import org.terracotta.coordination.Barrier;
 
@@ -23,7 +25,7 @@ public class PlayingWithToolkitMapOfLocks {
 
 		final String barrierName = "mapOfLocksBarrier";
 
-		final int numberOfParties = Integer.parseInt(args.length == 0 ? "2"
+		final int numberOfParties = Integer.parseInt(args.length == 0 ? "4"
 				: args[0]);
 		TerracottaClient client = new TerracottaClient("localhost:9510");
 		ClusteringToolkit clustering = client.getToolkit();
@@ -31,19 +33,23 @@ public class PlayingWithToolkitMapOfLocks {
 		ClusteredMap<String, Object> map = clustering.getMap("MyMap");
 		Barrier barrier = clustering.getBarrier(barrierName, numberOfParties);
 		ReadWriteLock l = clustering.getReadWriteLock(clustering
-				.getClusterInfo().getUniversallyUniqueClientID());
-		map.put(clustering.getClusterInfo().getUniversallyUniqueClientID(), l);
+				.getClusterInfo().getCurrentNode().getId());
+		map.put(clustering.getClusterInfo().getCurrentNode().getId(), l);
 
 		try {
 			System.out.println("Waiting ...");
 			int index = barrier.await();
-			Set<String> keys = (Set<String>) map.keySet();
-			for (String k : keys) {
-
-				ReadWriteLock mrwl = (ReadWriteLock) map.get(k);
-
-				System.out.println("MAP Locked: " + k + ": "
-						+ mrwl.writeLock().tryLock(1, TimeUnit.SECONDS));
+			Random r = new Random();
+			Collection<ClusterNode> keys = clustering.getClusterInfo()
+					.getClusterTopology().getNodes();
+			for (ClusterNode k : keys) {
+				Thread.sleep(1000 + r.nextInt(2000));
+				ReadWriteLock mrwl = (ReadWriteLock) map.get(k.getId());
+				boolean winner = mrwl.writeLock().tryLock(1,
+						TimeUnit.MILLISECONDS);
+				System.out.println("MAP Locked: " + k.getId() + ": " + winner);
+				if (winner)
+					break;
 			}
 			barrier.await();
 			System.out.println("... finished " + index);
